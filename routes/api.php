e38ee5ee27dbb5296ccf010a0da0cd56b1bf18eb
yasminermaili97 <yasminermaili6@gmail.com>
2025-11-16 15:17:03 +0100

    first commit

--- routes/api.php
+++ routes/api.php
@@ -14,24 +14,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-// Rutas de proyectos
+    // Rutas de proyectos
     Route::get('/projects', [ProjectController::class, 'misproyectos']);
+    Route::get('/projects/sorted-by-end-date', [ProjectController::class, 'sortedByEndDate']);
     Route::get('/projects/{id}', [ProjectController::class, 'showproyecto']);
     Route::post('/projects', [ProjectController::class, 'addproyectos']);
     Route::post('/projects/{id}/members', [ProjectController::class, 'addMembers']);
     Route::delete('/projects/{id}', [ProjectController::class, 'eliminarproyecto']);
-    Route::get('/projects/sorted-by-end-date', [ProjectController::class, 'sortedByEndDate']);
 
 
-// Rutas de tareas
+    // Rutas de tareas
     Route::get('/tasks', [TaskController::class, 'tareas']);
     Route::post('/tasks', [TaskController::class, 'addtareas']);
     Route::get('/tasks/{id}', [TaskController::class, 'showtask']);
     Route::put('/tasks/{id}', [TaskController::class, 'updatetask']);
     Route::delete('/tasks/{id}', [TaskController::class, 'deletetask']);
     Route::post('/tasks/{taskId}/assign', [TaskController::class, 'assignUser']);
-    
-// Rutas de Notas
+
+    // Rutas de Notas
     Route::get('/notes', [NoteController::class, 'notes']);
     Route::post('/notes', [NoteController::class, 'addnote']);
     Route::get('/notes/{id}', [NoteController::class, 'shownote']);
@@ -39,7 +39,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/notes/{id}', [NoteController::class, 'destroynote']);
     Route::get('/notes/shared', [NoteController::class, 'shared']);
 
-// Rutas de sesiones de tiempo
+    // Rutas de sesiones de tiempo
     Route::post('/time-sessions/start', [TimeSessionController::class, 'start']);
     Route::post('/time-sessions/stop', [TimeSessionController::class, 'stop']);
     Route::get('/time-sessions', [TimeSessionController::class, 'index']);
